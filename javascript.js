7f9af1dfcfb8ac6965654980206ea340e68fe92f
Angel Nunez <joseagnunez@gmail.com>
2023-01-13 08:13:51 -0800

    add eventlistner to grids that change color when hovered

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ function generateRandomColor(){
     randomNumber = randomNumber.toString(16);
     let randColor = randomNumber.padStart(6, 0);   
     return `#${randColor.toUpperCase()}`
-}
+};
 
 // 16x6 grid
 let sixteenBySixteenGrid = document.createElement('div');
@@ -27,13 +27,38 @@ while (currentRow < 16) {
         let createGrid = document.createElement('div');
         createGrid.className = `row-${currentRow}-grid`;
         createGrid.id = `grid(${iterator})`;
-        createGrid.textContent = 'div';
-        createGrid.style.backgroundColor = `${generateRandomColor()}`;
+        createGrid.style.width = '25px';
+        createGrid.style.height = '25px';
+        createGrid.style.backgroundColor = 'black';
+        createGrid.addEventListener('mouseover', (e) => {createGrid.style.backgroundColor = `${generateRandomColor()}`;});
         createRow.appendChild(createGrid);
     };
 
 };
 
+// header 
+let header = document.createElement('div');
+header.className = 'header';
+header.style.backgroundColor = '#e6380e';
+
+// header logo
+let headerLogo = document.createElement('h1');
+headerLogo.id = 'headerLogo';
+headerLogo.textContent = 'Etch-A-Sketch';
+
+// navigation
+let navigationDiv = document.createElement('div');
+navigationDiv.className = 'navigationDiv';
+
+
 // container div
+//contains all content
 let container = document.getElementById('container');
+container.style.display = 'flex';
+container.style.flexDirection = 'column';
+// header
+container.appendChild(header);
+header.appendChild(headerLogo);
+//navigation
+container.appendChild(navigationDiv);
 container.appendChild(sixteenBySixteenGrid);
