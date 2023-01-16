3eeb839d6acf9fe86ad2ecd1616a6fa0447b968b
Angel Nunez <joseagnunez@gmail.com>
2023-01-16 13:02:46 -0800

    Added basic structure of Etch-A-Sketch game Implemented basic Etch-a-Sketch functionality. Implemented a random color generator function that changes the square's color upon hover. Added a container for options on the left side of the grid." Also added basic styling for elements using CSS and Google Fonts for styling text."

--- javascript.js
+++ javascript.js
@@ -8,161 +8,89 @@ function generateRandomColor(){
     return `#${randColor.toUpperCase()}`
 };
 
-// // grid
-// let grid = document.createElement('div');
-// grid.className = 'grid';
-// grid.id = 'grid';
-// let currentRow = 0;
+//documentBody
+let DocumentBody = document.body;
 
-// while (currentRow < 130) {
-//     currentRow++;
-//     let createRow = document.createElement('div');
-//     createRow.className = `row-${currentRow}`;
-//     createRow.style.display = 'flex';
-//     grid.appendChild(createRow);
-
-//     let iterator = 0;
-//     while (iterator <250) {
-//         iterator++;
-//         let createGrid = document.createElement('div');
-//         createGrid.className = `row-${currentRow}-grid`;
-//         createGrid.id = `grid(${iterator})`;
-//         createGrid.style.display = 'flex';
-//         createGrid.style.flexGrow = '1';
-//         createGrid.style.height = '5px';
-//         createGrid.style.backgroundColor = 'black';
-//         createGrid.addEventListener('mouseover', (e) => {
-//             createGrid.style.backgroundColor = `${generateRandomColor()}`;
-//         });
-//         createRow.appendChild(createGrid);
-//     };
-
-// };
-
-let grid = document.createElement('div');
-grid.className = 'grid';
-grid.id = 'grid';
-
-grid.style.display = 'grid';
-grid.style.gridTemplateColumns = 'repeat(100, minmax(1px, 1fr))';
-grid.style.gridTemplateRows = 'repeat(77, minmax(9px, 1fr))';
-
-for (let i = 0; i < 100*77; i++) {
-  let createGrid = document.createElement('div');
-  createGrid.className = `grid-item`;
-  createGrid.id = `grid(${i})`;
-  createGrid.style.backgroundColor = 'black';
-  createGrid.addEventListener('mouseover', (e) => {
-      createGrid.style.backgroundColor = `${generateRandomColor()}`;
-  });
-  grid.appendChild(createGrid);
-};
-
-// header 
+//header
 let header = document.createElement('div');
 header.className = 'header';
-header.style.backgroundColor = '#e6380e';
-header.style.margin = 'none';
+DocumentBody.appendChild(header);
+header.style.display = 'flex'
+header.style.justifyContent = 'center';
 
-// header logo
-let headerLogo = document.createElement('h1');
-headerLogo.id = 'headerLogo';
-headerLogo.textContent = 'Etch-A-Sketch';
-headerLogo.style.fontSize = '60px';
-headerLogo.style.fontFamily = 'Rougant';
-headerLogo.style.color = 'white';
-headerLogo.style.marginLeft = '40px';
-headerLogo.style.marginTop = '25px';
-headerLogo.style.marginBottom = '15px';
-
-// navigation
-let navigationDiv = document.createElement('div');
-navigationDiv.className = 'navigationDiv';
-navigationDiv.style.display = 'flex';
-navigationDiv.style.justifyContent = 'space-around';
-let navigationElement1 = document.createElement('h2');
-navigationElement1.textContent = 'Placeholder';
-let navigationElement2 = document.createElement('h2');
-navigationElement2.textContent = 'Placeholder';
-let navigationElement3 = document.createElement('h2');
-navigationElement3.textContent = 'Placeholder';
-let navigationElement4 = document.createElement('h2');
-navigationElement4.textContent = 'Placeholder';
-let navigationElement5 = document.createElement('h2');
-navigationElement5.textContent = 'Placeholder';
-
-// bodyContainer
+//body container
 let bodyContainer = document.createElement('div');
 bodyContainer.className = 'bodyContainer';
-bodyContainer.style.display ='flex';
-bodyContainer.style.minHeight = '100vh'
-bodyContainer.style.marginLeft = '20px';
-// bodyContainer.style.marginRight = '20px';
+DocumentBody.appendChild(bodyContainer);
+bodyContainer.style.display = 'flex';
+bodyContainer.style.justifyContent = 'center';
 
+let headerText = document.createElement('h1');
+headerText.textContent = 'ETCH-A-SKETCH';
+headerText.style.fontFamily = 'Press Start 2P';
+headerText.style.color = '#FF00FF';
+header.appendChild(headerText);
 
-// optionsContainer
+//options container
 let optionsContainer = document.createElement('div');
 optionsContainer.className = 'optionsContainer';
-optionsContainer.style.display = 'flex';
-optionsContainer.style.flexDirection = 'column';
-optionsContainer.style.width = '500px';
-optionsContainer.style.border = '4px solid #fd7e4e';
-optionsContainer.style.borderTopLeftRadius = '20px';
-optionsContainer.style.borderBottomLeftRadius = '20px';
-optionsContainer.style.marginBottom = '70px';
-
-
-// expand icon container
-let expandContainer = document.createElement('div');
-expandContainer.className = 'expandContainer';
-expandContainer.style.display = 'flex';
-expandContainer.style.justifyContent = 'center';
-
-
-// expand icon
-let expand = document.createElement('img');
-expand.src = './images/sort.png';
-expand.style.height = '20px';
-expand.style.width = '20px';
-expand.style.margin = '10px';
-expandContainer.appendChild(expand);
-
-// etchContainer
-let etchContainer = document.createElement('div');
-etchContainer.className = 'etchContainer';
-etchContainer.style.backgroundColor = 'white';
-etchContainer.style.width = '100%';
-etchContainer.style.display = 'flex'
-etchContainer.style.flexDirection = 'column';
-etchContainer.style.borderTop = '4px solid #fd7e4e';
-etchContainer.style.paddingLeft = '50px';
-etchContainer.style.paddingTop = '50px';
-// etchContainer.style.overflow = 'hidden';
-
-
-// purple seperator
-let purpleSeperator = document.createElement('div');
-purpleSeperator.style.height = '15px';
-purpleSeperator.style.backgroundColor = '#964dfe';
-
-// container div
-//contains all content
-let container = document.getElementById('container');
-container.style.display = 'flex';
-container.style.flexDirection = 'column';
-// header
-container.appendChild(header);
-header.appendChild(headerLogo);
-//navigation
-container.appendChild(navigationDiv);
-navigationDiv.appendChild(navigationElement1);
-navigationDiv.appendChild(navigationElement2);
-navigationDiv.appendChild(navigationElement3);
-navigationDiv.appendChild(navigationElement4);
-navigationDiv.appendChild(navigationElement5);
-container.appendChild(bodyContainer);
 bodyContainer.appendChild(optionsContainer);
-bodyContainer.appendChild(etchContainer);
-etchContainer.appendChild(grid);
-etchContainer.appendChild(expandContainer);
-etchContainer.appendChild(purpleSeperator);
+optionsContainer.style.height = '690px';
+optionsContainer.style.width = '345px';
+optionsContainer.style.backgroundColor = '#0d1d26';
+optionsContainer.style.marginRight = '80px';
+optionsContainer.style.filter = 'drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2))';
+
+//grid container
+let gridContainer = document.createElement('div');
+gridContainer.className = 'gridContainer';
+bodyContainer.appendChild(gridContainer);
+gridContainer.style.display = 'flex';
+gridContainer.style.justifyContent = 'center';
+
+//grid
+let grid = document.createElement('div');
+grid.className = 'grid';
+grid.style.display = 'grid';
+grid.style.height = '640px';
+grid.style.width = '640px';
+grid.style.gridTemplateColumns = 'repeat(16, 40px)';
+grid.style.filter = 'drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2))';
+grid.style.border = '25px solid #0d1d26';
+let gridCount = 0;
+let gridIterator = 0;
+
+while (gridIterator < 16) {
+    gridIterator++;
+
+    while (gridCount < 16) {
+        gridCount++
+        let gridSquares = document.createElement('div');
+        gridSquares.className = 'gridSquares'
+        gridSquares.id = `gridSqaure #${gridCount} row #${gridIterator}`;
+        gridSquares.style.width = '40px';
+        gridSquares.style.height = '40px';
+        gridSquares.style.backgroundColor  = `white`;
+        gridSquares.style.boxSizing = 'border-box';
+
+        if (gridCount === 16) {
+            gridSquares.style.borderRight = '1px solid rgb(156, 156, 156)';
+        };
+
+        if (gridIterator === 16) {
+            gridSquares.style.borderBottom = '1px solid rgb(156, 156, 156)';
+        };
+
+        gridSquares.style.borderTop = '1px solid rgb(156, 156, 156)';
+        gridSquares.style.borderLeft = '1px solid rgb(156, 156, 156)';
+        gridSquares.addEventListener('mouseover', (e) => {
+            gridSquares.style.backgroundColor = `${generateRandomColor()}`;
+        });
+
+        grid.appendChild(gridSquares);
+    };
+    
+    gridCount = 0;
+};
+
+gridContainer.appendChild(grid);
