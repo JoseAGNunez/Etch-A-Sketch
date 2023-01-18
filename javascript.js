37868317dbb4bd4a42ce8c4c2f69192e2dbda8a6
Angel Nunez <joseagnunez@gmail.com>
2023-01-17 21:37:08 -0800

    add grid size slider and a clear button feature

--- javascript.js
+++ javascript.js
@@ -17,6 +17,8 @@ header.className = 'header';
 DocumentBody.appendChild(header);
 header.style.display = 'flex'
 header.style.justifyContent = 'center';
+header.style.marginTop = '40px';
+header.style.marginBottom = '40px';
 
 //body container
 let bodyContainer = document.createElement('div');
@@ -25,21 +27,65 @@ DocumentBody.appendChild(bodyContainer);
 bodyContainer.style.display = 'flex';
 bodyContainer.style.justifyContent = 'center';
 
-let headerText = document.createElement('h1');
+let headerText = document.createElement('p');
 headerText.textContent = 'ETCH-A-SKETCH';
 headerText.style.fontFamily = 'Press Start 2P';
 headerText.style.color = '#FF00FF';
+headerText.style.fontSize = '30px';
 header.appendChild(headerText);
 
 //options container
 let optionsContainer = document.createElement('div');
 optionsContainer.className = 'optionsContainer';
 bodyContainer.appendChild(optionsContainer);
-optionsContainer.style.height = '690px';
+optionsContainer.style.height = '704px';
 optionsContainer.style.width = '345px';
 optionsContainer.style.backgroundColor = '#0d1d26';
-optionsContainer.style.marginRight = '80px';
+optionsContainer.style.marginRight = '150px';
+optionsContainer.style.boxSizing = 'border-box';
+optionsContainer.style.padding = '32px';
 optionsContainer.style.filter = 'drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2))';
+optionsContainer.style.display = 'flex';
+optionsContainer.style.flexDirection = 'column';
+
+//gridSizeSlider
+let gridSize = 16;
+let gridSizeSliderContainer = document.createElement('div');
+gridSizeSliderContainer.className = 'gridSizeSliderContainer';
+gridSizeSliderContainer.style.marginBottom = '30px';
+let gridSizeSlider = document.createElement('input');
+gridSizeSlider.type = 'range';
+gridSizeSlider.min = '1';
+gridSizeSlider.max = '100';
+gridSizeSlider.value = '16';
+gridSizeSlider.id = 'gridSizeSlider';
+gridSizeSlider.addEventListener('change', function (e){
+    gridSize = document.getElementById('gridSizeSlider').value;
+    px = 640 / gridSize;
+    gridContainer.innerHTML = '';
+    ETCH();
+});
+gridSizeSliderContainer.appendChild(gridSizeSlider);
+optionsContainer.appendChild(gridSizeSliderContainer);
+
+
+//clearButton
+let clearButton = document.createElement('button');
+clearButton.id = 'clearButton';
+optionsContainer.appendChild(clearButton);
+clearButton.textContent = 'Clear';
+clearButton.style.border = '1px solid #FF00FF';
+clearButton.style.borderRadius = '3.5px';
+clearButton.style.backgroundColor = '#0d1d26';
+clearButton.style.color = '#FF00FF';
+clearButton.style.padding = '8px';
+clearButton.style.fontSize = '15px';
+clearButton.style.fontFamily = 'Roboto';
+clearButton.addEventListener('click', function(e){
+    gridSize = document.getElementById('gridSizeSlider').value;
+    gridContainer.innerHTML = '';
+    ETCH();
+});
 
 //grid container
 let gridContainer = document.createElement('div');
@@ -49,35 +95,37 @@ gridContainer.style.display = 'flex';
 gridContainer.style.justifyContent = 'center';
 
 //grid
+let px = 640 / gridSize;
 let grid = document.createElement('div');
 grid.className = 'grid';
 grid.style.display = 'grid';
 grid.style.height = '640px';
 grid.style.width = '640px';
-grid.style.gridTemplateColumns = 'repeat(16, 40px)';
+grid.style.gridTemplateColumns = `repeat(${gridSize}, ${px}px)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, ${px}px)`;
 grid.style.filter = 'drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2))';
-grid.style.border = '25px solid #0d1d26';
+grid.style.border = '32px solid #0d1d26';
 let gridCount = 0;
 let gridIterator = 0;
 
-while (gridIterator < 16) {
+while (gridIterator < gridSize) {
     gridIterator++;
 
-    while (gridCount < 16) {
+    while (gridCount < gridSize) {
         gridCount++
         let gridSquares = document.createElement('div');
         gridSquares.className = 'gridSquares'
         gridSquares.id = `gridSqaure #${gridCount} row #${gridIterator}`;
-        gridSquares.style.width = '40px';
-        gridSquares.style.height = '40px';
+        gridSquares.style.width = `${px}px`;
+        gridSquares.style.height = `${px}px`;
         gridSquares.style.backgroundColor  = `white`;
         gridSquares.style.boxSizing = 'border-box';
 
-        if (gridCount === 16) {
+        if (gridCount == gridSize) {
             gridSquares.style.borderRight = '1px solid rgb(156, 156, 156)';
         };
 
-        if (gridIterator === 16) {
+        if (gridIterator == gridSize) {
             gridSquares.style.borderBottom = '1px solid rgb(156, 156, 156)';
         };
 
@@ -94,3 +142,55 @@ while (gridIterator < 16) {
 };
 
 gridContainer.appendChild(grid);
+
+function ETCH() {
+    //grid
+let px = 640 / gridSize;
+let grid = document.createElement('div');
+grid.className = 'grid';
+grid.style.display = 'grid';
+grid.style.height = '640px';
+grid.style.width = '640px';
+grid.style.gridTemplateColumns = `repeat(${gridSize}, ${px}px)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, ${px}px)`;
+grid.style.filter = 'drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.2))';
+grid.style.border = '32px solid #0d1d26';
+let gridCount = 0;
+let gridIterator = 0;
+
+while (gridIterator < gridSize) {
+    gridIterator++;
+
+    while (gridCount < gridSize) {
+        gridCount++
+        let gridSquares = document.createElement('div');
+        gridSquares.className = 'gridSquares'
+        gridSquares.id = `gridSqaure #${gridCount} row #${gridIterator}`;
+        gridSquares.style.width = `${px}px`;
+        gridSquares.style.height = `${px}px`;
+        gridSquares.style.backgroundColor  = `white`;
+        gridSquares.style.boxSizing = 'border-box';
+        
+
+        if (gridCount == gridSize) {
+            gridSquares.style.borderRight = '1px solid rgb(156, 156, 156)';
+        };
+
+        if (gridIterator == gridSize) {
+            gridSquares.style.borderBottom = '1px solid rgb(156, 156, 156)';
+        };
+
+        gridSquares.style.borderTop = '1px solid rgb(156, 156, 156)';
+        gridSquares.style.borderLeft = '1px solid rgb(156, 156, 156)';
+        gridSquares.addEventListener('mouseover', (e) => {
+            gridSquares.style.backgroundColor = `${generateRandomColor()}`;
+        });
+
+        grid.appendChild(gridSquares);
+    };
+    
+    gridCount = 0;
+};
+
+gridContainer.appendChild(grid);
+};
